eb60ab2839c7424df864dfa061b905ce5640bd8f
Niccolò Grete <N@MacBook-Air-2.local>
2024-11-28 17:54:02 +0100

    load.json function added

--- gb_assistant/pdf_extractor/data.py
+++ gb_assistant/pdf_extractor/data.py
@@ -38,3 +38,14 @@ def save_pdf(content, title):
         f.write(content)
 
     print("Downloaded", title, " rulebook!")
+
+
+def load_json(file_path): #file path of JSON file to be processed needs to be given
+    """Opens JSON file and loads its content into a Python dictionary."""
+    try:
+        with open(file_path, 'r') as json_file: #opens JSON file, given with file_path where the file is stored
+            data = json.load(json_file) #loads the JSON file into a Python dictonary
+        return data #returns a Python dictonary of the JSON file
+    except Exception as e: #in case JSON file cannot be read, will throw an error
+        print(f"Error reading JSON file: {e}")
+        return None

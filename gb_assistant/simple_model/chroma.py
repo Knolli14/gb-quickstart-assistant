d724e9c6a842bf2e5475e7e8a7828fe2521f8819
amine layadi <amine.layadi@protonmail.com>
2024-11-29 17:58:12 +0100

    functionalized LLM

---
+++ gb_assistant/simple_model/chroma.py
@@ -0,0 +1,18 @@
+import chromadb
+
+
+def setup_chromadb(collection_name:str, embeddings, metadatas, documents):
+    client = chromadb.PersistentClient(path="/Users/alayadi/code/Knolli14/gb-quickstart-assistant/vector_store")
+    collection = client.create_collection(name=collection_name)
+    collection.add(
+        embeddings=embeddings,
+        metadatas=metadatas,
+        documents=documents,
+        ids=[str(i) for i in range(len(embeddings))],
+        )
+    return collection
+
+def get_collection():
+    client = chromadb.PersistentClient(path="/Users/alayadi/code/Knolli14/gb-quickstart-assistant/vector_store")
+    collection = client.get_collection()
+    return collection

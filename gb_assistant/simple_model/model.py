ab4421c8cda712f1c8eb9d71038a3a4f75730ec3
Kiril Korzh <korzhkir@gmail.com>
2024-12-03 15:57:49 +0100

    adding updated model

--- gb_assistant/simple_model/model.py
+++ gb_assistant/simple_model/model.py
@@ -2,6 +2,8 @@ from transformers import pipeline
 import gb_assistant.params as params
 import PyPDF2
 import os
+import chromadb
+from sentence_transformers import SentenceTransformer
 
 game_file = params.GAME_NAME + ".pdf"
 abs_path = os.path.join(os.getcwd(), "raw_data", game_file)
@@ -22,13 +24,34 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
         return text
 
+def load_llm():
+    """Loads the final LLM."""
+    model_name = "google/flan-t5-small"
+    qa_pipe = pipeline("text2text-generation", model=model_name)
+    return qa_pipe
+
 
 def answer_question(model, question:str):
     """Answer the question passed as input"""
-    context = extract_text_from_pdf(abs_path)# Extract context from the PDF
-    answer = model(question=question, context=context, max_answer_len=50, top_k=5)
-    best_answer=""
-    for a in answer:
-        if len(best_answer) < len(a.get('answer')):
-            best_answer=a.get('answer')
-    return best_answer
+    vector_store_path = "/Users/kirillkorzh/code/Knolli14/gb-quickstart-assistant/vector_store"  # Update this path to your PDF directory
+    client = chromadb.PersistentClient(vector_store_path)
+    collection = client.get_collection(
+        name="gb-assistant-complete"
+        )
+    print(collection.count())
+    # Load the LLM
+    qa_pipe = load_llm()
+
+    # Test the LLM with a sample query
+    query = "What are the rules for scoring in this game?"
+    embed_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    embedded_query = embed_model.encode(query)
+    query_results = collection.query(query_embeddings=embedded_query,n_results=3)
+
+    context = "\n\n".join(query_results["documents"][0])
+    prompt = context+" "+query
+    response = qa_pipe(prompt, max_length=100)
+
+    return response
+
+print(answer_question(load_model(),"how to win?"))

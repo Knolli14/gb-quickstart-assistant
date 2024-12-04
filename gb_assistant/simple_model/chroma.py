6b4fe57181b904a4d29fc039dce9d73c517eaa1b
Oliver Reichelt <olli.coding@proton.me>
2024-12-04 12:02:28 +0100

    added workflow to get jsons from bucket

--- gb_assistant/simple_model/chroma.py
+++ gb_assistant/simple_model/chroma.py
@@ -16,3 +16,36 @@ def get_collection():
     client = chromadb.PersistentClient(path="/Users/alayadi/code/Knolli14/gb-quickstart-assistant/vector_store")
     collection = client.get_collection()
     return collection
+
+
+
+
+#new by Olli
+def create_index(game_chunks, collection):
+    for chunk in game_chunks:
+        collection.add(
+            ids=chunk[0],
+            documents=chunk[1],
+            embeddings=chunk[2]
+        )
+
+def create_collection(path, title, dfunc="cosine"):
+
+    client = chromadb.PersistentClient(path)
+    collection = client.get_or_create_collection(
+        name=title,
+        metadata={"hnsw:space": dfunc}
+    )
+    return collection
+
+
+def setup_database(path, title, game_chunks, dfunc="cosine"):
+
+    collection = create_collection(
+        path=path,
+        title=title
+    )
+
+    vindex = create_index(game_chunks, collection)
+
+    return vindex
